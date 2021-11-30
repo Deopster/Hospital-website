50e38c95b9db3379ed9552a3e033deb02f6c9ac1
Deopster <68296704+Deopster@users.noreply.github.com>
2021-11-30 19:33:04 +0300

    admin

---
+++ ajax.php
@@ -0,0 +1,11 @@
+<?php
+require_once("connect.php");
+$link = mysqli_connect($host, $user, $password, $db_name) or die(mysqli_error($link));
+mysqli_query($link, "SET NAMES 'utf8'");
+$val = $_POST['fn'];
+$id = $_POST['fr'];
+$pieces = explode(" ", $id);
+$finr=$pieces[1];
+$finn=$pieces[0];
+mysqli_query($link, "UPDATE users SET $finr=('$val') WHERE id='$finn'") or die(mysqli_error($link)); 
+?>
